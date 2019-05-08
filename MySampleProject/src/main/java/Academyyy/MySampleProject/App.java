491295bfd06e619425a26df7586db7f9ce9d475b
MayuriLakshmi <durga.mayuri74@gmail.com>
2019-05-09 00:02:24 +0530

    sysouts added

---
+++ MySampleProject/src/main/java/Academyyy/MySampleProject/App.java
@@ -0,0 +1,15 @@
+package Academyyy.MySampleProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
