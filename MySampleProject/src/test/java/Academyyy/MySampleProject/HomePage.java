2e2d8845ee2eb4030c29c1e22582663653d598d9
MayuriLakshmi <durga.mayuri74@gmail.com>
2019-05-09 17:46:07 +0530

    parameterisedvaues by dataprovider

--- MySampleProject/src/test/java/Academyyy/MySampleProject/HomePage.java
+++ MySampleProject/src/test/java/Academyyy/MySampleProject/HomePage.java
@@ -2,7 +2,7 @@ package Academyyy.MySampleProject;
 
 import java.io.IOException;
 
-
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
@@ -10,17 +10,32 @@ import pageObjects.LoginPage;
 import resources.Base;
 
 public class HomePage  extends Base{
-	@Test
-	public void basePageNavigation() throws IOException {
+	@Test(dataProvider="getData")
+	public void basePageNavigation(String username,String password,String text) throws IOException {
 		driver=initialiseDriver();
 		driver.get("http://qaclickacademy.com");
 		LandingPage l=new LandingPage(driver);
 		l.getLogIn().click();
 		LoginPage lp=new LoginPage(driver);
-		lp.getEmail().sendKeys("jhiugjag@gmail.com");
-		lp.getPassword().sendKeys("hdgdnddm");
-		lp.getLoginAccount().click();
+		lp.getEmail().sendKeys(username);
+		lp.getPassword().sendKeys(password);
+		System.out.println(text);
 		
+		lp.getLoginAccount().click();
+	}
+	@DataProvider
+	public Object[][] getData() {
+		Object[][] data=new Object[2][3];
+		
+		data[0][0]="nonrestricted@gmail.com";
+		data[0][1]="35678858";
+		data[0][2]="ehry";
+		
+		data[1][0]="restricted@gmail.com";
+		data[1][1]="35678858gnhmmrjjgrhtr";
+		data[1][2]="ehryhhjrgfn";
+		
+		return data;
 		
 		
 	}
